474dd1de31396908e5d7d4bd3da4914d62e17119
Adolfo <adolfo.sobrino@gmail.com>
2021-02-21 20:25:07 +0100

    Version comentada

--- geometria/src/Rectangulo_ASG.java
+++ geometria/src/Rectangulo_ASG.java
@@ -1,19 +1,36 @@
 
+/**
+ * @author Adolfo Sobrino Gomez
+ * @version 1.2
+ */
 public class Rectangulo_ASG extends FiguraGeometrica_ASG {
 	private double l1;
 	private double l2;
 	
+	/**
+	 * Constructor de la clase rectangulo.
+	 * @param tipoFigura
+	 * @param lG
+	 * @param lP
+	 */
 	public Rectangulo_ASG(String tipoFigura, double lG, double lP) {
 		super(tipoFigura);
 		l1 = lG;
 		l2 = lP;
 	}
 
+	/**
+	 * Este metodo calcula el area del rectangulo multiplicando lado por lado.
+	 * @return el area del rectangulo
+	 */
 	@Override
 	public double area() {
 		return l1 * l2;
 	}
-	
+	/**
+	 * Este metodo calcula el perimetro del rectangulo sumando todos sus lados.
+	 * @return el perimetro del rectangulo
+	 */
 	@Override
 	public double perimetro() {
 		return (2 * l1) + (2 * l2);
