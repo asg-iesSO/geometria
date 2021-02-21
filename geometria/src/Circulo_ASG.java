bc06f91998889e953b5534a25e6caa72e24a145e
Adolfo <adolfo.sobrino@gmail.com>
2021-02-21 20:27:30 +0100

    Siempre positivo

--- geometria/src/Circulo_ASG.java
+++ geometria/src/Circulo_ASG.java
@@ -6,6 +6,9 @@ public class Circulo_ASG extends FiguraGeometrica_ASG {
 	
 	public Circulo_ASG(double r, String tipoFigura) {
 		super(tipoFigura);
+		if(r<0) {
+			r = Math.abs(r);
+		}
 		radio = r;
 	}
 
